12f2b5252b877634373c045bca79b63394130166
sds62866 <sds62866@gamil.com>
2025-08-08 17:46:48 +0900

    질문 수정

--- src/main/java/AIServlet.java
+++ src/main/java/AIServlet.java
@@ -34,7 +34,7 @@ public class AIServlet extends HttpServlet { // [1]
                 .apiKey(apiKey).build(); // [9]
         // gemini-2.0-flash
         String data = client.models.generateContent("gemini-2.0-flash",
-                        "오늘 저녁 메뉴 추천해줘. 결과만 작성해줘. 마크다운 혹은 꾸미는 문법 없이 평문으로. 100자 이내로 작성해줘.", null)
+                        "오늘 미국증시현황 알려줘. 결과만 작성해줘. 마크다운 혹은 꾸미는 문법 없이 평문으로. 100자 이내로 작성해줘.", null)
                 .text(); // text를 불러와줌. [10]
 //        req.setAttribute("data", "안녕하세요! 반갑습니다!"); // [6]
         req.setAttribute("data", data); // [11]
